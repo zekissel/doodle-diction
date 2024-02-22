fb8a405b718b10b40607cece891bb64e88d0e46b
Zane Kissel <zekissel@gmail.com>
2024-02-22 13:52:52 -0500

    fix frontend dependencies

--- dd-client/.eslintrc.cjs
+++ dd-client/.eslintrc.cjs
@@ -11,12 +11,26 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json', './tsconfig.node.json'],
-    tsconfigRootDir: __dirname,
-  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/strict-type-checked',
+        'plugin:@typescript-eslint/stylistic-type-checked',
+        'plugin:react-hooks/recommended',
+        'plugin:react/recommended',
+        'plugin:react/jsx-runtime',
+      ],
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: ['./tsconfig.json', './tsconfig.node.json'],
+        tsconfigRootDir: __dirname,
+      },
+   },
+  ],
+  
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
