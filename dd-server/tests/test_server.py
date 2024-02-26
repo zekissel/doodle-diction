d2e39a09d04c93bf0ff3ad59a48165d086e73b28
Zane Kissel <zekissel@gmail.com>
2024-02-26 02:47:33 -0500

    ordered server tests

--- dd-server/tests/test_server.py
+++ dd-server/tests/test_server.py
@@ -3,14 +3,15 @@ import requests
 import time
 from ..schema import User, Room, index_models
 
-class TestAPIServer(unittest.TestCase):
+unittest.TestLoader.sortTestMethodsUsing = None
+
+class TestAPIServerEmpty(unittest.TestCase):
 
     def setUp(self):
-        self.user = User(uID='0', name='test-host', ready=False)
-        self.user.save()
+        pass
 
     def tearDown(self):
-        User.delete(self.user.pk)
+        pass
     
     def test_get_public_rooms_none(self):
         """
@@ -20,22 +21,51 @@ class TestAPIServer(unittest.TestCase):
         """
         r = requests.get("http://localhost:8000/join").json()
         self.assertEqual(r['rooms'], [])
+
+class TestAPIServer(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
     
-    def test_get_public_rooms_some(self):
+    def test_host_public_room(self):
         """
-        *  GIVEN the server is running and some rooms exist
-        *  WHEN a GET request is made to '/join'
-        *  THEN the server should return a list of public rooms
+        *  GIVEN the server is running and no rooms exist
+        *  WHEN a POST request is made to '/host' with a room name and host name
+        *  THEN the server should return a r_key, u_key, and u_id
         """
-        room = Room(name='test-room', cap=4, host=self.user, rID=0)
-        room.save()
-        time.sleep(1)
+        data = { 'name': 'Test Room', 'host': 'Test Host' }
+        r = requests.post("http://localhost:8000/host", json=data).json()
+        self.assertEqual(r['u_id'], 0)
+        self.assertIn('r_key', r.keys())
+        self.assertIn('u_key', r.keys())
+        self.host_key = r['u_key']
+        self.room_key = r['r_key']
 
-        r = requests.get("http://localhost:8000/join").json()
-        self.assertEqual(len(r['rooms']), 1)
+    def test_join_public_room(self):
+        """
+        *  GIVEN the server is running and one room exists
+        *  WHEN a POST request is made to '/join' with a room name and user name
+        *  THEN the server should return a r_key, u_key, and u_id
+        """
+        data = { 'name': 'Test Room', 'host': 'Test User', 'pw': '' }
+        r = requests.post("http://localhost:8000/join", json=data).json()
+        self.assertEqual(r['u_id'], 1)
+        self.assertIn('r_key', r.keys())
+        self.assertIn('u_key', r.keys())
+        self.assertNotEqual(self.host_key, r['u_key'])
+        self.user_key = r['u_key']
 
-        Room.delete(room.pk)
-
+    def test_close_room(self):
+        """
+        *  GIVEN the server is running and one room exists
+        *  WHEN a POST request is made to '/exit' with the room and host keys
+        *  THEN the server should return a 200 status code
+        """
+        r = requests.post("http://localhost:8000/exit", json={ 'r_key': self.room_key, 'u_key': self.host_key }).json()
+        self.assertEqual(r.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
