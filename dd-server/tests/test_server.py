97dc6bdb64707c68dfa26486ddccb30f5fd4681c
Zane Kissel <zekissel@gmail.com>
2024-02-26 02:24:38 -0500

    small api tests

--- dd-server/tests/test_server.py
+++ dd-server/tests/test_server.py
@@ -17,9 +17,8 @@ class TestAPIServer(unittest.TestCase):
         *  WHEN a GET request is made to '/join'
         *  THEN the server should return an empty list
         """
-        r = requests.get("http://localhost:8000/join")
-        print(r.text)
-        pass
+        r = requests.get("http://localhost:8000/join").json()
+        self.assertEqual(r['rooms'], [])
     
     def test_get_public_rooms_some(self):
         """
@@ -27,7 +26,13 @@ class TestAPIServer(unittest.TestCase):
         *  WHEN a GET request is made to '/join'
         *  THEN the server should return a list of public rooms
         """
-        pass
+        room = Room(name='test-room', cap=4, host=self.user, rID=0)
+        room.save()
+
+        r = requests.get("http://localhost:8000/join").json()
+        self.assertEqual(len(r['rooms']), 1)
+
+        Room.delete(room.pk)
 
 
 if __name__ == '__main__':
