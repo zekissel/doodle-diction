3b4e2262e1c763dfe298a1828373d3490099f2dc
Zane Kissel <zekissel@gmail.com>
2024-02-26 02:18:45 -0500

    test api

--- dd-server/tests/test_server.py
+++ dd-server/tests/test_server.py
@@ -1,4 +1,5 @@
 import unittest
+import requests
 from ..schema import User, Room, index_models
 
 class TestAPIServer(unittest.TestCase):
@@ -8,23 +9,26 @@ class TestAPIServer(unittest.TestCase):
         self.user.save()
 
     def tearDown(self):
-        pass
+        User.delete(self.user.pk)
     
     def test_get_public_rooms_none(self):
         """
-        *  GIVEN a the server is running and no rooms exist
-        *  WHEN a a GET request is made to '/join'
+        *  GIVEN the server is running and no rooms exist
+        *  WHEN a GET request is made to '/join'
         *  THEN the server should return an empty list
         """
+        r = requests.get("http://localhost:8000/join")
+        print(r.text)
         pass
     
     def test_get_public_rooms_some(self):
         """
-        *  GIVEN a the server is running and some rooms exist
-        *  WHEN a a GET request is made to '/join'
+        *  GIVEN the server is running and some rooms exist
+        *  WHEN a GET request is made to '/join'
         *  THEN the server should return a list of public rooms
         """
         pass
+
 
 if __name__ == '__main__':
     unittest.main()
